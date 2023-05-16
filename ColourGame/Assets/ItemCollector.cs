252046ced7025cb12541584ab11c6aead7d2b113
Jawass242 <129192024+Jawass242@users.noreply.github.com>
2023-05-16 16:22:21 +0100

    Finished collection

--- ColourGame/Assets/ItemCollector.cs
+++ ColourGame/Assets/ItemCollector.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-   private int Bananas = 0;
+    private int Bananas = 0;
+
+    [SerializeField] private Text BananasText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Banana"))
         {
             Destroy(collision.gameObject);
             Bananas++;
-            Debug.Log("Bananas: " + Bananas);
+            BananasText.text = "Bananas: " + Bananas;
         }
     }
 }
